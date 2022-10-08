94516fe1a86958504a1edb0362074c8d7eadfcff
ekanata14 <ekanata1411@gmail.com>
2022-10-08 12:54:02 +0800

    commit

---
+++ back-end-dicoding/Object/object.js
@@ -0,0 +1,44 @@
+// Object is a data type that has key-value pair
+
+// Function for calling the object
+function myComp(){
+    console.log(`My computer has ${computer.cpu} cpu, ${computer.gpu} gpu, ${computer.ram} ram and ${computer.ssd}`);
+}
+
+// Function for deleting the object
+function deleteObjProperty(object, property){
+    delete object[property]
+}
+
+// Declaring an object
+const computer = {
+    cpu : 'i7',
+    gpu : 'rtx 3060',
+    ram : '32gb',
+    ssd : '512gb',   
+}
+
+// Calling the object
+
+myComp();
+
+// Changing the object
+computer.cpu = 'i9';
+computer.gpu = 'rtx 3090';
+computer.ram = '64gb';
+computer.ssd = '1tb';
+
+// Calling the object
+myComp();
+
+//Calling delete object property function
+deleteObjProperty(computer, 'cpu');
+
+// calling the object
+myComp();
+
+// calling delete object property function
+deleteObjProperty(computer, 'gpu');
+
+// calling the object
+myComp();
