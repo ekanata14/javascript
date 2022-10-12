753910b6f3497530515720b055835e9d76d9ec48
ekanata14 <ekanata1411@gmail.com>
2022-10-12 21:20:05 +0800

    Dicoding test and quiz

--- back-end-dicoding/Function/function.js
+++ back-end-dicoding/Function/function.js
@@ -97,3 +97,46 @@ console.log(add());
 console.log(add());
 console.log(add());
 
+console.log("Dicoding code test");
+
+function minimal(a,b){
+    if(a < b){
+        return a;
+    } else if(b < a){
+        return b;
+    } else{
+        return a;
+    }
+}
+
+console.log(minimal(20,50));
+
+function power(a,b){
+    return a ** b;
+}
+
+console.log(power(10,2));
+
+console.log('Dicoding quiz');
+
+function calculate(value){
+    return value < 2 ? value : (calculate(value - 1) + calculate(value - 2));
+}
+
+console.log(calculate(3));
+
+function minMax(arrayOfNumbers){
+    let currentMin = arrayOfNumbers[0];
+    let currentMax = arrayOfNumbers[0];
+    for(value of arrayOfNumbers){
+        if(value < currentMin){
+            currentMin = value;
+        } else if(value > currentMax){
+            currentMax = value;
+        }
+    }
+
+    console.log(`currentMin: ${currentMin}, currentMax: ${currentMax}`);
+}
+
+minMax([8, -6, 0, 9, 40, 2, 23, 50, 2, -3, -15, 15, -23, 71]);
